3f6b9b172f3580c58a6e50673fb3a71903ecb82e
Kohde Pitcher <kohde1@hotmail.com>
2021-01-11 16:06:24 +1000

    add http loading indicator for http requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { LoaderService } from "./services/http-loader.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'nominationsystem';
+
+export class AppComponent implements AfterViewInit {
+  title = 'RJC Jumpouts';
+
+  isLoading = false;
+
+  constructor(public loaderService: LoaderService) { }
+
+  ngAfterViewInit() {
+    // this.loaderService.httpProgress().subscribe((status: boolean) => {
+    //   if (status) {
+    //     // this.renderer.addClass(document.body, 'cursor-loader');
+    //     this.isLoading = true
+    //   } else {
+    //     // this.renderer.removeClass(document.body, 'cursor-loader');
+    //     this.isLoading = false
+    //   }
+    // });
+  }
 }
