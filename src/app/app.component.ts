dc974b3e723cece0db03b3916111dbed243f5c25
Kohde Pitcher <kohde1@hotmail.com>
2021-01-11 16:31:38 +1000

    added browser animation for loading spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,44 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { LoaderService } from "./services/http-loader.service";
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+
+  //animations for showing and hiding the progress bar
+  animations: [
+    trigger(
+      'inOutAnimation', 
+      [
+        transition(
+          ':enter', 
+          [
+            style({ opacity: 0 }),
+            animate('0.5s ease-out', 
+                    style({ opacity: 1 }))
+          ]
+        ),
+        transition(
+          ':leave', 
+          [
+            style({ opacity: 1 }),
+            animate('0.5s ease-in', 
+                    style({ opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ]
 })
 
 export class AppComponent implements AfterViewInit {
   title = 'RJC Jumpouts';
 
   isLoading = false;
+
+  
 
   constructor(public loaderService: LoaderService) { }
 
