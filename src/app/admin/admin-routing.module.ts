7597ec52903d2d0da3c3a40c2c313ee1c8389720
Kohde Pitcher <kohde1@hotmail.com>
2021-01-13 00:12:05 +1000

    added nominations page to admin routing

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminPageComponent } from './admin-page/admin-page.component';
-import { AllNominationsComponent } from './all-nominations/all-nominations.component';
+import { AllNominationsComponent } from './nominations/all-nominations-for-jumpout/all-nominations.component';
 import { UserManagementComponent } from './user-management/user-management.component';
+import { NominationsComponent } from "./nominations/nominations.component";
 
 import { AdminGuard } from './../user/admin.guard';
 
@@ -10,7 +11,8 @@ const routes: Routes = [
   //root page path
   { path: '', component: AdminPageComponent},
   { path: 'all-nominations', component: AllNominationsComponent, canActivate:[AdminGuard], data: {roles: ['admin']} },
-  { path: 'user-management', component: UserManagementComponent, canActivate:[AdminGuard], data: {roles: ['admin']} }
+  { path: 'user-management', component: UserManagementComponent, canActivate:[AdminGuard], data: {roles: ['admin']} },
+  { path: 'nominations', component: NominationsComponent, canActivate:[AdminGuard], data: {roles: ['admin']} }
 ];
 
 @NgModule({
